f187ff8c0cea45c2a1fe93a30110614b14c5072b
Demian <aimedemian@gmail.com>
2024-01-22 15:34:56 -0300

    primer test base

---
+++ src/components/Me.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Me() {
+  return (
+    <div className="section bg-red-800" id="huevo1">
+      huevo
+    </div>
+  );
+}
---
+++ src/components/Projects.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div className="section bg-blue-800" id="huevo2">
+      huevo
+    </div>
+  );
+}
