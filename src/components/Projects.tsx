4f0a44d628a7c17fb4fc2e84a08aa2ed687d37ee
Demian <aimedemian@gmail.com>
2024-01-29 13:50:59 -0300

    agregado monitor para desktop. mobile deberia mantenerse. test

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,11 +7,32 @@ export default function Projects() {
         className="w-10/12 md:w-2/3 h-2/3 p2 bg-red-300 shadow-xl relative"
         style={{ borderRadius: "4.5vh" }}
       >
+        <div className="hidden lg:flex bg-transparent h-48 w-[400px] absolute z-50 bottom-4 left-[38%] text-center flex-col items-center justify-around">
+          <div className="text-5xl font-bold">MICROIMPLANT</div>
+          <p>
+            Sitio tipo catalogo desarrollado para empresa de implantes
+            ortopédicos
+          </p>
+          <div className="w-1/2 flex justify-evenly">
+            <img className="w-12 h-12" src="/assets/github.png" alt="" />
+            <img className="w-12 h-12" src="/assets/site.png" alt="" />
+          </div>
+        </div>
         <div className="phone-container">
           <div className="phone">
             <img src="/assets/microimplant_mobile.png" alt="" />
           </div>
         </div>
+        <div className="absolute left-[38%] -bottom-2 h-full">
+          <article className="hidden lg:block relative ">
+            <div className="stand">
+              <div className="monitor">
+                <img src="/assets/microimplant_desktop.png" />
+              </div>
+            </div>
+          </article>
+        </div>
+
         <ul className="absolute h-full -right-6 top-0 w-12 bg-transparent flex flex-col justify-around items-center">
           <li className="transition-all hover:scale-125">
             <img src="/assets/html.png" alt="" />
